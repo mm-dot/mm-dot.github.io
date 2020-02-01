6d0542a66ead2a0052fc5bbb9515f317ffb3c2f0
Michelle Martin <michellemartin@Michelles-iMac.local>
2020-02-01 17:23:28 -0500

    first commit

---
+++ home.php
@@ -0,0 +1,116 @@
+<main>
+      <section class="jumbotron">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-6">
+                    <img src="./img/martinmichelle.jpg" class="mx-auto d-block img-fluid mich-small" alt="...">
+              </div>
+              <div class="col-sm-6">
+                <div class="hybrid">
+                  <h1 class="display-4">Michelle Martin</h1>
+                  <h3 class="">Dev-Marketing Hybrid</h3>
+                  <!-- <p class="lead text-muted">Shopify Liquid, Wordpress PHP, CSS, JS, Landing Pages & Forms, HTML Emails, Analytics & Marketing Intel, PPC Campaigns, SEO, Agile Methodologies</p> -->
+                  <p class="lead text-muted">e-Commerce Developer, Growth-Hacker, all-around great gal </p>
+                  <a href="https://www.linkedin.com/in/michelleannmartin/" class="btn btn-secondary my-2">LinkedIn</a>
+                  <a href="https://github.com/mm-dot" class="btn btn-primary my-2">GitHub</a>
+                </div>
+              </div>
+          </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="work">
+        <div class="container justify-content-center align-items-center">
+            <div class="d-flex justify-content-center projects-header">
+                <h2>Projects</h2>
+                <p></p>
+            </div>
+
+          <div class="card-columns">
+
+            <div class="card h-100">
+              <img class="card-img-top" src="./img/mycro.jpg" alt="  ">
+              <div class="card-body">
+                <h5 class="card-title">Mycrobio.me Rails App</h5>
+                <p class="card-text">An interactive 3D model of human organ systems and their flora, as well as corresponding antibiotics. </p>
+              </div>
+              <div class="card-footer">
+                <small class="text-dark">JQuery | JS | Rails </small>
+              </div>
+            </div>
+
+            <div class="card h-100">
+              <img class="card-img-top" src="./img/mycro.jpg" alt="  ">
+              <div class="card-body">
+                <h5 class="card-title">Mycrobio.me Rails App</h5>
+                <p class="card-text">An interactive 3D model of human organ systems and their flora, as well as corresponding antibiotics. </p>
+              </div>
+              <div class="card-footer">
+                <small class="text-dark">JQuery | JS | Rails </small>
+              </div>
+            </div>
+
+              <div class="card h-100">
+                <img class="card-img-top" src="./img/lp.png" alt="  ">
+                <div class="card-body">
+                  <h5 class="card-title">eCommerce Wordpress - Shopify Integration</h5>
+                  <p class="card-text">Wordpress site populated by Shopify inventory, featuring secure forms and more.</p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-dark">Shopify API | WooCommerce | Wordpress </small>
+                </div>
+              </div>
+
+              <div class="card h-100">
+                <img class="card-img-top" src="./img/movieapp3.png" alt="  ">
+                <div class="card-body">
+                  <h5 class="card-title">React Mobile App</h5>
+                  <p class="card-text">An unpublished mobile web app for viewing films playing in theaters. Built with React Native for mobile support. </p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-dark">React | React Native | JS </small>
+                </div>
+              </div>
+
+
+              <div class="card h-100">
+                <img class="card-img-top" src="./img/jbd.png" alt="  ">
+                <div class="card-body">
+                  <h5 class="card-title">Custom Wordpress Theme w/ WooCommerce </h5>
+                  <p class="card-text">Custom Wordpress theme in production, made entirely from scratch with GULP. Includes JS features, and WooCommerce support.</p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-dark"> PHP | Bootstrap </small>
+                </div>
+              </div>
+
+                <div class="card h-100">
+                  <img class="card-img-top" src="./img/landing.png" alt="  ">
+                  <div class="card-body">
+                    <h5 class="card-title">Responsive Landing Page</h5>
+                    <p class="card-text">I've built many PSD-to-Responsive landing pages, including: http://inktel.com/tim, http://www.inktel.com/be-world-class/, https://www.idcomlog.com/tim/ </p>
+                  </div>
+                  <div class="card-footer">
+                    <small class="text-dark"> JS | Bootstrap</small>
+                  </div>
+                </div>
+
+
+
+                <div class="card h-100">
+                  <img class="card-img-top" src="./img/kluger-email.png" alt="  ">
+                  <div class="card-body">
+                    <h5 class="card-title">HTML Emails</h5>
+                    <p class="card-text">Custom Wordpress theme in production, made entirely from scratch with GULP. Includes JS features, and WooCommerce support.</p>
+                  </div>
+                  <div class="card-footer">
+                    <small class="text-dark">Email Design | HTML</small>
+                  </div>
+                </div>
+
+            </div>
+          </div>
+      </section>
+
+    </main>
---
+++ resume.php
@@ -0,0 +1,10 @@
+<?php include("nav.php"); ?>
+<div class="center_content">
+    <object height="900" data="MM-resume-2020.pdf#zoom=80" type="application/pdf" width="70%">
+      <p>It appears you don't have a PDF plugin for this browser.
+        No biggie... you can <a href="MichelleMartin_Resumé_2018.pdf">click here to
+        download the PDF file.</a>
+      </p>
+    </object>
+</div>
+<?php include("footer.php"); ?>
